ca8b8abc7c55c0ee7cfac4c24042334f45f9024d
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-04 14:58:51 -0500

    Adds answers.

--- pin-code.js
+++ pin-code.js
@@ -1,3 +1,22 @@
 const getInput = require('./get-input.js');
 
 // Your code here!
+
+let guessedCorrectly = false;
+let numberOfGuess = 0;
+let iteration = 1;
+
+while(!guessedCorrectly && getInput(iteration) !== undefined){
+    if(getInput(iteration) === '1234'){
+        guessedCorrectly = true;
+        numberOfGuess++;
+        console.log('That is correct. You took ' + numberOfGuess + ' guess(es).')
+    }else{
+        numberOfGuess++;
+        iteration++;
+    }
+}
+
+if(guessedCorrectly === false){
+    console.log('None of the entered pin codes are correct.');
+}
--- sum-args.js
+++ sum-args.js
@@ -1,3 +1,15 @@
 const getInput = require('./get-input.js');
 
 // Your code here!
+
+function addInputNumbers(){
+    let x = 0;
+    let count = 1;
+    while(getInput(count) !== undefined){
+        x += Number(getInput(count));
+        count++;
+    }
+    console.log(x);
+}
+
+addInputNumbers();
